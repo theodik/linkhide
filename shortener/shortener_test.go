aa93f23d781e4a79ebbc9eea4627e911d8ba99bd
Ondřej Svoboda <theodik@gmail.com>
2017-06-12 10:30:42 +0200

    Retab

--- shortener/shortener_test.go
+++ shortener/shortener_test.go
@@ -3,23 +3,23 @@ package shortener
 import "testing"
 
 func TestFnv1a(t *testing.T) {
-	if n := fnv1a("node.js"); n != 3096844302 {
-		t.Error("Expected 3096844302 got ", n)
-	}
+  if n := fnv1a("node.js"); n != 3096844302 {
+    t.Error("Expected 3096844302 got ", n)
+  }
 }
 
 func TestBase62(t *testing.T) {
-	if str := base62(3096844302); str != "dxKcPO" {
-		t.Error("Expected dxKcPO got", str)
-	}
+  if str := base62(3096844302); str != "dxKcPO" {
+    t.Error("Expected dxKcPO got", str)
+  }
 }
 
 func TestEncode(t *testing.T) {
-	id := fnv1a("http://minecraft.project-nemesis.cz/")
-	//3860153612
-	short := base62(id)
+  id := fnv1a("http://minecraft.project-nemesis.cz/")
+  //3860153612
+  short := base62(id)
 
-	if enc := Encode("http://minecraft.project-nemesis.cz/"); enc != short {
-		t.Error("Expected ", short, " got ", enc)
-	}
+  if enc := Encode("http://minecraft.project-nemesis.cz/"); enc != short {
+    t.Error("Expected ", short, " got ", enc)
+  }
 }
